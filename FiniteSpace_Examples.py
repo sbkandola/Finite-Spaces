becfe8ff8213a96a9f709f7d4a15fd314328fb9e
Liz Munch <muncheli@msu.edu>
2020-02-05 14:42:44 -0500

    Changed example file to be function based

--- FiniteSpace_Examples.py
+++ FiniteSpace_Examples.py
@@ -9,58 +9,76 @@ Created on Wed Feb  5 10:33:45 2020
 
 import FiniteSpaces_Class as FS
 
-Klein = FS.FiniteSpace(dict({
-        'a1': set({'a1', 'b1', 'b2','b3','b4','c1','c2','c3','c4'}),
-        'a2': set({'a2', 'b1', 'b2','b5','b6','c1','c2','c3','c4'}),
-        'a3': set({'a3', 'b3', 'b5','b7','b8','c1','c2','c3','c4'}),
-        'a4': set({'a4', 'b4', 'b6','b7','b8','c1','c2','c3','c4'}),
-        'b1': set({'b1','c1','c3'}),
-        'b2': set({'b2','c2','c4'}),
-        'b3': set({'b3','c3','c4'}),
-        'b4': set({'b4','c1','c2'}),
-        'b5': set({'b5','c1','c2'}),
-        'b6': set({'b6','c3','c4'}),
-        'b7': set({'b7','c1','c3'}),
-        'b8': set({'b8','c2','c4'}),
-        'c1': set({'c1'}),
-        'c2': set({'c2'}),
-        'c3': set({'c3'}),
-        'c4': set({'c4'})}))
+def Build_Klein():
+    Klein = FS.FiniteSpace(dict({
+            'a1': set({'a1', 'b1', 'b2','b3','b4','c1','c2','c3','c4'}),
+            'a2': set({'a2', 'b1', 'b2','b5','b6','c1','c2','c3','c4'}),
+            'a3': set({'a3', 'b3', 'b5','b7','b8','c1','c2','c3','c4'}),
+            'a4': set({'a4', 'b4', 'b6','b7','b8','c1','c2','c3','c4'}),
+            'b1': set({'b1','c1','c3'}),
+            'b2': set({'b2','c2','c4'}),
+            'b3': set({'b3','c3','c4'}),
+            'b4': set({'b4','c1','c2'}),
+            'b5': set({'b5','c1','c2'}),
+            'b6': set({'b6','c3','c4'}),
+            'b7': set({'b7','c1','c3'}),
+            'b8': set({'b8','c2','c4'}),
+            'c1': set({'c1'}),
+            'c2': set({'c2'}),
+            'c3': set({'c3'}),
+            'c4': set({'c4'})}))
+    return Klein
 
-MinCircle = FS.FiniteSpace(dict({'a': set({'a','c','d'}),
-       'b': set({'b','c','d'}), 
-       'c': set({'c'}), 
-       'd': set({'d'})}))
+def Build_MinCircle():
+    MinCircle = FS.FiniteSpace(dict({'a': set({'a','c','d'}),
+           'b': set({'b','c','d'}),
+           'c': set({'c'}),
+           'd': set({'d'})}))
+    return MinCircle
 
-SixCircle = FS.FiniteSpace(dict({
-        'y0': set({'y0','x0','x1'}),
-        'y1': set({'y1','x1','x2'}),
-        'y2': set({'y2','x2','x0'}),
-        'x0': set({'x0'}),
-        'x1': set({'x1'}),
-        'x2': set({'x2'})
-    }))
+def Build_SixCircle():
 
-TenCircle = FS.FiniteSpace(dict({
-        'y0': set({'y0','x0','x1'}),
-        'y1': set({'y1','x1','x2'}),
-        'y2': set({'y2','x2','x3'}),
-        'y3': set({'y3','x3','x4'}),
-        'y4': set({'y4','x4','x0'}),
-        'x0': set({'x0'}),
-        'x1': set({'x1'}),
-        'x2': set({'x2'}),
-        'x3': set({'x3'}),
-        'x4': set({'x4'})
-    }))
+    SixCircle = FS.FiniteSpace(dict({
+            'y0': set({'y0','x0','x1'}),
+            'y1': set({'y1','x1','x2'}),
+            'y2': set({'y2','x2','x0'}),
+            'x0': set({'x0'}),
+            'x1': set({'x1'}),
+            'x2': set({'x2'})
+        }))
+    return SixCircle
 
-MinTorus = MinCircle.product(MinCircle)
+def Build_TenCircle():
+    TenCircle = FS.FiniteSpace(dict({
+            'y0': set({'y0','x0','x1'}),
+            'y1': set({'y1','x1','x2'}),
+            'y2': set({'y2','x2','x3'}),
+            'y3': set({'y3','x3','x4'}),
+            'y4': set({'y4','x4','x0'}),
+            'x0': set({'x0'}),
+            'x1': set({'x1'}),
+            'x2': set({'x2'}),
+            'x3': set({'x3'}),
+            'x4': set({'x4'})
+        }))
+    return TenCircle
 
-SixTorus = SixCircle.product(SixCircle)
+def Build_MinTorus():
+    MinTorus = MinCircle.product(MinCircle)
+    return MinTorus
 
-TenTorus = TenCircle.product(TenCircle)
+def Build_SixTorus():
+    SixTorus = SixCircle.product(SixCircle)
+    return SixTorus
 
-KleinSquared = Klein.product(Klein)
+def Build_TenTorus():
+    TenTorus = TenCircle.product(TenCircle)
+    return TenTorus
 
+def Build_KleinSquared():
+    KleinSquared = Klein.product(Klein)
+    return KleinSquared
 
-print(Klein.opens)
+if __name__=='__main__':
+    Klein = Build_Klein()
+    print(Klein.opens)
