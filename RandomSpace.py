5f4ed39c418ec012969aaa702ab6941eadd597ae
Shelley B. Kandola <31456063+sbkandola@users.noreply.github.com>
2021-01-19 11:06:09 -0500

    Create RandomSpace.py
    Added a file for trying to create random finite spaces using an Erdos-Renyi inspired method

---
+++ RandomSpace.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 14:35:15 2021
+
+@author: shell
+"""
+
+
+import networkx as nx
+import numpy as np
+import operator
+import random
+import FiniteSpaces_Class as FS
+
+   
+v  = 10
+p=.2
+
+
+def randomSpace(points, probability):
+    v = points
+    p = probability
+    
+    # add the nodes
+    space = nx.DiGraph()
+    space.add_nodes_from(range(v))
+    
+    # establish the height dictionary
+    height_dict = {}
+    for i in range(v):
+        height_dict[i] = 0
+        
+    for point1 in space.nodes:
+       for point2 in space.nodes:
+           if (height_dict[point2]==0 or height_dict[point1] > height_dict[point2]) and (point1 != point2):
+               if random.random()<p:
+                   space.add_edge(point1, point2)
+                   height_dict[point1] = height_dict[point2] + 1
+                   
+    space = nx.transitive_reduction(space)
+    Space = FS.FiniteSpace(space)
+    return Space
+   
